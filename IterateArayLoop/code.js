a400b0b93439eeb5118aee0f144dd657d96861e6
Tatiana Kilishek <tatiana.kilishek@gmail.com>
2023-04-20 14:27:01 -0400

    With all comments

--- IterateArayLoop/code.js
+++ IterateArayLoop/code.js
@@ -4,7 +4,16 @@ const myArr = [2, 3, 4, 5, 6];
 // Only change code below this line
 let total = 0; // Declatred and Initialized variable total to 0
 
-for (let i = 0; i < myArr.length; i++){  
-  total += myArr[i];  
+for (let i = 0; i < myArr.length; i++){
+  console.log('---Start of iteration---: ' + (i+1))
+  console.log('Total now: ' + total);
+  console.log('Current value of "i": ' + i);
+  console.log('myArr[i] now: ' + myArr[i]);
+
+  total += myArr[i]; 
+  
+  console.log('Total now: ' + total); 
+  console.log('Current value of "i": ' + i);
+  console.log('---End of iteration---\n')
 }
 console.log(total);
